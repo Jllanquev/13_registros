ebe55fadb1d02611dbc7a9b8e8c4d3a7e0f79d9a
jllanquev <jllanquev@unjbg.edu.pe>
2025-06-25 20:27:19 -0500

    ACTUALIZACION: se le agrego al programa que pueda modificar el usuario en el registro

--- contactos.cpp
+++ contactos.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream>
 using namespace std;
 
@@ -48,7 +47,6 @@ int main(){
                 
                 leerCorreo(email,user,domain);
                 leerContacto(cont,nom,sex,edad,email);
-                //imprimeContacto(cont);
 
                 lista[n] = cont;
                 n++;
@@ -56,33 +54,82 @@ int main(){
                 break;
             case 2:
                 for(int i = 0; i < n; i++){
-                    cout<<"Contacto #"<<i<<endl;
+                    cout<<"Contacto #"<<i+1<<endl;
                     imprimeContacto(lista[i]);
                     cout<<endl;
                 }
                 system("pause");
                 break;
             case 3:
-            	for(int i=0;i<n;i++){
-            		cout<<"Contacto #"<<i<<endl;
-				}
-            	cout<<"Que contancto desea modificar: ";cin>>mod;
-                cout<<"Contacto #"<<mod<<endl;
-                imprimeContacto(lista[mod]);
-                cout<<endl;	
-				
-				cout<<"Ingrese los datos de un usuario: "<<endl;
-                cin.ignore();
-                cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
-                cout<<"Ingrese el sexo (M/F): "; cin>>sex;
-                cout<<"Ingrese la edad: "; cin>>edad;
-                cout<<"Ingrese el correo electronico (usuario@dominio): "<<endl;
-                cout<<"\tIngrese el usuario: "; cin>>user;
-                cout<<"\tIngrese el dominio: "; cin>>domain;	
-				system("pause");
+			    for(int i = 0; i < n; i++){
+			        cout<<"Contacto #"<<i+1<<" - "<<lista[i].nom<<endl;
+			    }
+			    cout<<"Que contacto desea modificar: ";
+			    cin >> mod;
+				mod--;
+			    if(mod >= 0 && mod < n){
+			        int campo;
+			        do {
+			            system("cls");
+			            cout<<"Contacto actual:"<<endl;
+			            imprimeContacto(lista[mod]);
+			            cout<<"\n¿Qué desea modificar?"<<endl;
+			            cout<<"1. Nombre"<<endl;
+			            cout<<"2. Sexo"<<endl;
+			            cout<<"3. Edad"<<endl;
+			            cout<<"4. Usuario del correo"<<endl;
+			            cout<<"5. Dominio del correo"<<endl;
+			            cout<<"0. Volver al menu principal"<<endl;
+			            cout<<"Seleccione una opción: ";
+			            cin >> campo;
+			
+			            cin.ignore(); 
+			
+			            switch(campo){
+			                case 1:
+			                    cout<<"Nuevo nombre: ";
+			                    getline(cin, lista[mod].nom);
+			                    break;
+			                case 2:
+			                    cout<<"Nuevo sexo (M/F): ";
+			                    cin >> lista[mod].sex;
+			                    break;
+			                case 3:
+			                    cout<<"Nueva edad: ";
+			                    cin >> lista[mod].edad;
+			                    break;
+			                case 4:
+			                    cout<<"Nuevo usuario del correo: ";
+			                    cin >> lista[mod].email.user;
+			                    break;
+			                case 5:
+			                    cout<<"Nuevo dominio del correo: ";
+			                    cin >> lista[mod].email.domain;
+			                    break;
+			                case 0:
+			                    cout<<"Volviendo al menu principal..."<<endl;
+			                    break;
+			                default:
+			                    cout<<"Opcion no valida."<<endl;
+			            }
+			            system("pause");
+			        } while(campo != 0);
+			    } else {
+			        cout<<"Indice no valido."<<endl;
+			        system("pause");
+			    }
+			    break;
 				     
             case 0:
+            	char Finish;
                 cout<<"Esta seguro de salir? (S/N): ";
+                cin>>Finish;
+                if(Finish=='S'||Finish=='s'){
+                	cout<<"Cerrando programa...";
+				}
+				else{
+					op=-1;
+				}
                 break;
             default:
                 cout<<"Opcion no valida!"<<endl;
